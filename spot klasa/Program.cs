0b2c8d53254a66255e5abf9317af7a9bdf438281
a <145933200+ivasosic@users.noreply.github.com>
2024-01-23 10:38:56 +0100

    Add files via upload

---
+++ spot klasa/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spot_klasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sport Nogomet = new sport("Nogomet", true, 22 );
+            sport Curling = new sport("Curling", true, 4);
+            sport Vaterpolo = new sport("Vaterpolo", false, 14);
+
+            Console.WriteLine(Nogomet.ToString());
+            Console.WriteLine(Curling.ToString());
+            Console.WriteLine(Vaterpolo.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ spot klasa/sport.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spot_klasa
+{
+    internal class sport
+    {
+        String naziv;
+        Boolean IgraliseLoptom;
+        int BrojIgraca;
+
+        public string Naziv { get => naziv; set => naziv = value; }
+        public bool IgraliseLoptom1 { get => IgraliseLoptom; set => IgraliseLoptom = value; }
+        public int BrojIgraca1 { get => BrojIgraca; set => BrojIgraca = value; }
+
+        public sport(string naziv, bool igraliseLoptom, int brojIgraca)
+        {
+            this.naziv = naziv;
+            IgraliseLoptom = igraliseLoptom;
+            BrojIgraca = brojIgraca;
+        }
+
+        public override string ToString()
+        {
+            string str = "\nNaziv sporta: " + naziv + "\n Igrali se loptom: "+IgraliseLoptom+ "\n Broj igrača: " +BrojIgraca;
+            return str;
+        }
+    }
+    
+    
+
+}
